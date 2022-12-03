94df7453360707778c8712f91d49fb269d64b544
Md. Rafiqul Islam <rafiqul@linkstaff.co.jp>
2022-12-03 20:14:07 +0600

    protect routes

--- src/App.js
+++ src/App.js
@@ -6,11 +6,59 @@ import {
   RouterProvider,
   Route,
   Link,
+  Outlet,
+  Navigate,
 } from "react-router-dom";
 import Register from './pages/register/Register';
+import ProfilePage from './pages/profile/ProfilePage';
+import Navbar from './components/navbar/Navbar'
+import LeftBar from './components/leftbar/LeftBar'
+import RightBar from './components/rightbar/RightBar'
+import HomePage from './pages/homepage/HomePage';
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar/>
+      <div style={{display: 'flex'}}>
+        <LeftBar/>
+        <Outlet/>
+        <RightBar/>
+
+      </div>
+    </div>
+  );
+}
 
 function App() {
+
+  //Declare protected routes
+  const ProtectedRoute = ({children}) => {
+    let currentUser = true;
+    if(!currentUser){
+      return <Navigate to='/login' />
+    }else{
+      return children;
+    }
+  }
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: 
+      <ProtectedRoute>
+         <Layout />
+      </ProtectedRoute>,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/profile/:id",
+          element: <ProfilePage />,
+        },
+      ]
+    },
     {
       path: "/login",
       element: <Login />,
