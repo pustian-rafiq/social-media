b6f9c7e1832626aa5d6e4e84388b325d631a1772
Md. Rafiqul Islam <rafiqul@linkstaff.co.jp>
2022-12-03 21:19:40 +0600

    navbar design

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,34 @@
 import "./navbar.scss"
+import HomeIcon from '@mui/icons-material/Home';
+import { Link } from "react-router-dom";
+import { DarkModeOutlined, EmailOutlined, GridViewOutlined, HomeOutlined, NotificationsOutlined, PersonOutlined, SearchOutlined } from "@mui/icons-material";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar">
+      <div className="left">
+        <Link to="/" style={{ textDecoration:'none' }}>
+          <span>Social Media</span>
+        </Link>
+        <HomeOutlined/>
+        <DarkModeOutlined/>
+        <GridViewOutlined/>
+        <div className="search">
+          <SearchOutlined/>
+          <input type="text" placeholder="Search here..." />
+        </div>
+      </div>
+      <div className="right">
+          <PersonOutlined/>
+          <EmailOutlined/>
+          <NotificationsOutlined/>
+          <div className="user">
+            <img src="/images/login.jpg" alt="Profile" />
+            <span>Rafiqul Islam</span>
+          </div>
+      </div>
+       
+    </div>
   )
 }
 
