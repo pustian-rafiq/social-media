136c76450d6f7a708588399dbfe4e7a1617370f4
Md. Rafiqul Islam <rafiqul@linkstaff.co.jp>
2022-12-23 20:58:10 +0600

    design post section

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Posts from '../../components/posts/Posts'
 import Stories from '../../components/stories/Stories'
 import "./home.scss"
 const HomePage = () => {
   return (
     <div className="home">
       <Stories/>
+      <Posts/>
     </div>
   )
 }
